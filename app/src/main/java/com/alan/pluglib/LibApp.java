30bcc20d8054efb32504273b82632c09c58100de
许君明 <gw00306182@tech.gwm.cn>
2022-09-05 15:33:17 +0800

    第一次提交

---
+++ app/src/main/java/com/alan/pluglib/LibApp.java
@@ -0,0 +1,12 @@
+package com.alan.pluglib;
+
+import android.app.Application;
+
+public class LibApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
---
+++ app/src/main/java/com/alan/pluglib/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alan.pluglib;
+
+import android.os.Bundle;
+
+import com.qihoo360.replugin.loader.a.PluginAppCompatActivity;
+
+public class MainActivity extends PluginAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
